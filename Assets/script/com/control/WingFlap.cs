3d0ed96c4f36380df9a60fba67fbc87ada668542
minuj <minujeong@maverickgames.co>
2015-01-24 15:27:03 +0900

    progress save

---
+++ Assets/script/com/control/WingFlap.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WingFlap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.RotateAdd (gameObject, iTween.Hash ("amount", new Vector3 (0, 0, 15),
+		                                           "easetype", iTween.EaseType.linear,
+		                                           "time", 1.5f,
+		                                           "looptype", iTween.LoopType.pingPong));
+	}
+}
