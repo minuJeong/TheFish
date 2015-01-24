728b798b64b0be2b4edf9054bedeb2b5161e8016
minuj <minujeong@maverickgames.co>
2015-01-24 16:06:51 +0900

    save progression

---
+++ Assets/script/com/control/WaterSurfaceWaving.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterSurfaceWaving : MonoBehaviour {
+	private void Start (){
+		iTween.ScaleAdd (gameObject, iTween.Hash ("amount", new Vector3 (-0.03f, 0.1f, 0f),
+		                                          "easetype", iTween.EaseType.linear,
+		                                          "looptype", iTween.LoopType.pingPong));
+	}
+}
