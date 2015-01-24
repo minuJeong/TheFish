1318c3d8ec596df1f03f53b9de10cde4b2c1bd4a
skyser2003 <shjang1992@gmail.com>
2015-01-24 15:25:52 +0900

    Book UI reads data from Book and displays it

---
+++ Assets/script/com/BookUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class BookUI : MonoBehaviour
+{
+    private UIGrid grid;
+    public void Init(Book book)
+    {
+        if(book.PawnInfoList.Count == 0)
+        {
+            return;
+        }
+
+        grid = transform.FindChild("Clip").FindChild("Foreground").FindChild("grid").gameObject.GetComponent<UIGrid>();
+        var item = grid.gameObject.transform.FindChild("item").gameObject;
+
+        foreach (var info in book.PawnInfoList)
+        {
+            // Add item object
+            var clone = (GameObject) UnityEngine.Object.Instantiate(item);
+            clone.transform.parent = grid.gameObject.transform;
+
+            clone.transform.localPosition = item.transform.localPosition;
+            clone.transform.localScale = item.transform.localScale;
+
+            var newPosition = clone.transform.localPosition;
+            newPosition.y += -275.24f;
+            clone.transform.localPosition = newPosition;
+
+            // Fill data
+            var name = clone.transform.FindChild("name_text").gameObject.GetComponent<UILabel>();
+            var rank = clone.transform.FindChild("rank_text").gameObject.GetComponent<UILabel>();
+
+            name.text = info.name;
+            rank.text = info.rank.ToString();
+        }
+
+        GameObject.Destroy(item.gameObject);
+    }
+}
