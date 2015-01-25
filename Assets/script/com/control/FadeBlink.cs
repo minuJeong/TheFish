77c12b2ff406d28c982be510ba24ec70d71cfeab
minuj <minujeong@maverickgames.co>
2015-01-25 16:45:07 +0900

    added new scenes: splash, intro

---
+++ Assets/script/com/control/FadeBlink.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeBlink : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine (StartFade ());
+	}
+
+	private IEnumerator StartFade ()
+	{
+		while (true) { 
+			TweenAlpha.Begin (gameObject, 0.5f, 0.0f);
+			yield return new WaitForSeconds (0.5f);
+			TweenAlpha.Begin (gameObject, 0.5f, 1.0f);
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+}
